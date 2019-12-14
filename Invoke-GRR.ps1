2dea0452d5c4f2f9433a1b4932b6230ba4fbf5e5
tbar317 <tombarnard81@gmail.com>
2019-12-14 16:15:12 -0500

    First commit of the Invoke-GRRAgentDeployer.ps1 script.

---
+++ Invoke-GRR.ps1
@@ -0,0 +1,20 @@
+﻿Function Invoke-DeployGRRAgent 
+{
+    [CmdletBinding()]
+    Param(
+    [alias("a")]
+    [String]$Agent,
+    [alias("u")]
+    [String]$username,
+    [alias("p")]
+    [SecureString]$password
+    )
+    
+    Enable-PSRemoting -SkipNetworkProfileCheck -Force
+
+        }
+}
+
+
+
+   
