c4d00d95e4a2a096969a89c2a40dbffad95ba7e4
tbar317 <tombarnard81@gmail.com>
2019-12-14 16:24:33 -0500

    Adding in some intial coding to identify the location of the GRR file and remote host to connect to.

--- Invoke-GRRAgentDeployer.ps1
+++ Invoke-GRRAgentDeployer.ps1
@@ -21,6 +21,29 @@
     { 
         Set-Item WSMan:\localhost\Client\TrustedHosts -Value * -Force 
     }
+
+     #Add a variable for credentials
+     $UserPassSecure = ConvertTo-SecureString $password -AsPlainText -Force
+     $UserCredentials = New-Object -TypeName System.Management.Automation.PSCredential $UserName,$UserPassSecure
+ 
+ 
+     Write-Host "Enter location of your GRR Executable: " -ForegroundColor Yellow
+     LocalPath = Read-Host  
+     Write-Host "The local path has been set to $LocalPath" -ForegroundColor Cyan
+     Write-Host "Enter the location on the remote host wher you want to deploy the executable: " -ForegroundColor Yellow
+     RemotePath = Read-Host 
+     Write-Host "The remote path has been set to $RemotePath" -ForegroundColor Cyan
+ 
+     Copy-Item -Path "C:\Users\DCI Student\Downloads\GRR_3.2.0.1_amd64.exe" "C:\Tools\dbg_GRR_3.2.0.1_amd64.exe"
+ 
+     $Session1 = New-PSSession -ComputerName 172.16.12.5 -Credential Administrator
+ 
+     Copy-Item -ToSession $Session1 -Path c:\Tools\dbg_GRR_3.2.0.1_amd64.exe -Destination c:\Tools\dbg_GRR_3.2.0.1_amd64.exe
+ 
+     Invoke-Command -ComputerName $Target -Credential $UserCredentials -ScriptBlock {
+         Start-Process $RemotePath\$Agent -NoNewWindow -PassThru 
+         Get-Process -Name GRR*
+     }
 }
 
 
