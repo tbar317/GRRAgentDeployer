ce5d02061f648d3c835a62e979e8c4a3c5379ef6
tbar317 <tombarnard81@gmail.com>
2019-12-14 16:29:38 -0500

    Update script with initial connection info for one host.

--- Invoke-GRRAgentDeployer.ps1
+++ Invoke-GRRAgentDeployer.ps1
@@ -11,7 +11,7 @@
     )
     
     Enable-PSRemoting -SkipNetworkProfileCheck -Force
-
+    #Set the Execution Policy if needed
     if((Get-ExecutionPolicy) -ne 'Unrestricted') 
     { 
         Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
@@ -22,28 +22,30 @@
         Set-Item WSMan:\localhost\Client\TrustedHosts -Value * -Force 
     }
 
-     #Add a variable for credentials
-     $UserPassSecure = ConvertTo-SecureString $password -AsPlainText -Force
-     $UserCredentials = New-Object -TypeName System.Management.Automation.PSCredential $UserName,$UserPassSecure
+    #Add a variable for credentials
+    $UserPassSecure = ConvertTo-SecureString $password -AsPlainText -Force
+    $UserCredentials = New-Object -TypeName System.Management.Automation.PSCredential $UserName,$UserPassSecure
  
+    #Provide the full path the GRR executable.
+    Write-Host "Enter location of your GRR Executable: " -ForegroundColor Yellow
+    LocalPath = Read-Host  
+    Write-Host "The local path has been set to $LocalPath" -ForegroundColor Cyan
+     
+    #Provide the location of where to set the GRR agent on the remote hosts
+    Write-Host "Enter the location on the remote host wher you want to deploy the executable: " -ForegroundColor Yellow
+    RemotePath = Read-Host 
+    Write-Host "The remote path has been set to $RemotePath" -ForegroundColor Cyan
+
+    #Establish a session to the remost host to move the executable to the remote system.
+    $Session1 = New-PSSession -ComputerName 172.16.12.5 -Credential Administrator
+    
+    #Use the Copy-Item function to move the executable across the established session
+    Copy-Item -ToSession $Session1 -Path c:\Tools\dbg_GRR_3.2.0.1_amd64.exe -Destination c:\Tools\dbg_GRR_3.2.0.1_amd64.exe
  
-     Write-Host "Enter location of your GRR Executable: " -ForegroundColor Yellow
-     LocalPath = Read-Host  
-     Write-Host "The local path has been set to $LocalPath" -ForegroundColor Cyan
-     Write-Host "Enter the location on the remote host wher you want to deploy the executable: " -ForegroundColor Yellow
-     RemotePath = Read-Host 
-     Write-Host "The remote path has been set to $RemotePath" -ForegroundColor Cyan
- 
-     Copy-Item -Path "C:\Users\DCI Student\Downloads\GRR_3.2.0.1_amd64.exe" "C:\Tools\dbg_GRR_3.2.0.1_amd64.exe"
- 
-     $Session1 = New-PSSession -ComputerName 172.16.12.5 -Credential Administrator
- 
-     Copy-Item -ToSession $Session1 -Path c:\Tools\dbg_GRR_3.2.0.1_amd64.exe -Destination c:\Tools\dbg_GRR_3.2.0.1_amd64.exe
- 
-     Invoke-Command -ComputerName $Target -Credential $UserCredentials -ScriptBlock {
-         Start-Process $RemotePath\$Agent -NoNewWindow -PassThru 
-         Get-Process -Name GRR*
-     }
+    Invoke-Command -ComputerName $Target -Credential $UserCredentials -ScriptBlock {
+        Start-Process $RemotePath\$Agent -NoNewWindow -PassThru 
+        Get-Process -Name GRR*
+    }
 }
 
 
