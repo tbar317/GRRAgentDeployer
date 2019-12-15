f877334652575a61af821d9fff241ec3b5636d7b
tbar317 <tombarnard81@gmail.com>
2019-12-14 21:29:29 -0500

    Updated credentials section, Setting the path to executable and command line args.

--- Invoke-GRRAgentDeployer.ps1
+++ Invoke-GRRAgentDeployer.ps1
@@ -2,16 +2,7 @@
 {
     [CmdletBinding()]
     Param(
-    [String]$target,
-    [alias("a")]
-    [Parameter(Mandatory=$true)]
-    [String]$agent,
-    [Parameter(Mandatory=$true)]
-    [alias("u")]
-    [String]$username,
-    [Parameter(Mandatory=$true)]
-    [alias("p")]
-    [SecureString]$password
+    [String]$target
     )
     
     Enable-PSRemoting -SkipNetworkProfileCheck -Force
@@ -20,54 +11,92 @@
     { 
         Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
     }
+
+    Write-Host "Execution Policy has been set to Bypass." -ForegroundColor Cyan
+    
     #Set the TrustedHosts Value
     if((Get-Item -Path WSMan:\localhost\Client\TrustedHosts).Value -ne $subnet) 
     { 
         Set-Item WSMan:\localhost\Client\TrustedHosts -Value * -Force 
     }
+    
+    Write-Host "Trusted Hosts values are set." -ForegroundColor Cyan
+    
+    Write-Host ""
+
+    Write-Host "Setting up Credentials to use on the remote host(s)..." -ForegroundColor Cyan
+
+    Write-Host ""
+
+    Write-Host "Enter the domain short name " -ForegroundColor Yellow
+
+    $Domain = Read-Host
+
+    Write-Host ""
+
+    Write-Host "Enter the username to connect to the remote system(s)" -ForegroundColor Yellow
+
+    $UserName = Read-Host 
+
+    $domainCreds = "$Domain\$Username"
+
+    Write-Host ""
+
+    $password = Read-Host "Please enter the password to connect to the remote system(s)" -AsSecureString
+
+    Write-Host ""
 
     #Add a variable for credentials
-    $UserPassSecure = ConvertTo-SecureString $password -AsPlainText -Force
-    $UserCredentials = New-Object -TypeName System.Management.Automation.PSCredential $UserName,$UserPassSecure
+
+    $UserPassSecure = $password 
+
+    $UserCredentials = New-Object -TypeName System.Management.Automation.PSCredential $domainCreds,$UserPassSecur
+
     
-    Write-Host "Do you want to identify a list of targets"
-    if ($target -eq 'null') {
+    Write-Host "Do you want to identify a list of targets: yes or no?" -ForegroundColor Yellow
+    $answer = Read-Host
+
+    if ($answer -eq 'yes') {
         #Load your hosts in a host file. Will update this in the future to be more dynamic.
         Write-Host "Enter the path to a list of hosts." -ForegroundColor Yellow
-        Read-Host = $RemoteHosts
+        Read-Host = $FilePath
+        $RemoteHosts = Get-Content @($FilePath)
     } 
     else {
-        $h = $target
+        New-Item -Name %temp%/remotehosts.txt -ItemType File
+        Write-Output $target > %temp%/remotehosts.txt
+        $RemoteHosts = Get-Content @(%temp%/remotehosts.txt)
     }
 
     #Provide the full path the GRR executable.
     Write-Host "Enter the full path to your GRR Executable: " -ForegroundColor Yellow
-    LocalPath = Read-Host  
+    $lpath = Read-Host  
     Write-Host "The local path has been set to $LocalPath" -ForegroundColor Cyan
      
     #Provide the location of where to set the GRR agent on the remote hosts
     Write-Host "Enter the full path on the remote host wher you want to deploy the executable: " -ForegroundColor Yellow
-    RemotePath = Read-Host 
+    $rpath = Read-Host 
     Write-Host "The remote path has been set to $RemotePath" -ForegroundColor Cyan
 
     foreach ($h in $RemoteHosts) {
         #Establish a session to the remost host to move the executable to the remote system.
-        $Session1 = New-PSSession -ComputerName $h -Credential Administrator
+        $Session1 = New-PSSession -ComputerName $h -Credential $UserCredentials
         
         #Use the Copy-Item function to move the executable across the established session
-        Copy-Item -ToSession $Session1 -Path $LocalPath -Destination $RemotePath
+        Copy-Item -ToSession $Session1 -Path $lpath -Destination $rpath
         #Kill the session
         Remove-PSSession -Session $Session1
         #Create a new connection, start the GRR agent, verify it, disconnect and kills session automatically.
         Invoke-Command -ComputerName $h -Credential $UserCredentials -ScriptBlock {
-            Start-Process $RemotePath -NoNewWindow -PassThru 
-            Get-Process -Name GRR*
+            Start-Process $using:rpath -NoNewWindow -PassThru 
+            #Get-Process -Name GRR*
+            Get-Process -Name PStest.exe
         }
-    }
 
-    Write-Host "GRR agent deployed for host $h" -ForegroundColor Cyan
+        Write-Host "GRR agent deployed for host $h" -ForegroundColor Cyan
+    }
 }
 
- 
+Invoke-DeployGRRAgent -target 192.168.131.101
 
    
